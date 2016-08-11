9ba2ed6f4d4897e0b2a9913f2f3b4d7837e3b8a4
bingoohuang <bingoo.huang@gmail.com>
2016-08-11 15:53:33 +0800

    tr_f_user sync

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-dbsync
-synchronize the database tables 
+synchronize the database tables
+
+## V1
+只能同步tr_f_user表的数据。
+同步原理：比较两边数据，没有的补充上，有的但是不一样的，控制台打印。
+用法:
+
+`./user-sync "root:my-secret-pw@tcp(192.168.99.100:13306)/dba" "root:my-secret-pw@tcp(192.168.99.100:13306)/dbb"`
---
+++ src/user-sync.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"fmt"
+	"reflect"
+	"os"
+)
+
+type User struct {
+	userId, mobile, openid sql.NullString
+	merchantId, createTime sql.NullString
+	effective              sql.NullString
+	comparedState          uint8
+	comparedResult         *string
+}
+
+// user-sync "root:my-secret-pw@tcp(192.168.99.100:13306)/dba" "root:my-secret-pw@tcp(192.168.99.100:13306)/dbb"
+func main() {
+	dataSourceName1 := os.Args[1]
+	db1 := getDb(dataSourceName1)
+	defer db1.Close()
+
+	dataSourceName2 := os.Args[2]
+	db2 := getDb(dataSourceName2)
+	defer db2.Close()
+
+	users1 := queryRows(db1)
+	mapUsers1 := makeMap(users1)
+	users2 := queryRows(db2)
+	mapUsers2 := makeMap(users2)
+
+	compare(mapUsers1, mapUsers2)
+
+	merge(db1, mapUsers2)
+	merge(db2, mapUsers1)
+}
+
+func merge(db *sql.DB, users map[string]*User) {
+	for _, user := range users {
+		if user.comparedState == 1 {
+			// 一边有另外一边没有
+			insertUser(db, user)
+		}
+	}
+}
+
+func compare(users1 map[string]*User, users2 map[string]*User) {
+	for userId, user1 := range users1 {
+		user2, ok := users2[userId]
+		if !ok {
+			user1.comparedState = 1 // 左边有右边没有
+			continue
+		}
+
+		// 两边都有, 比较内容
+		deep_equal := reflect.DeepEqual(user1, user2)
+		if deep_equal {
+			user1.comparedState = 2
+			user2.comparedState = 2
+		} else {
+			user1.comparedState = 3
+			user2.comparedState = 3
+			comparedResult := fmt.Sprintf("<<<\n%s\n>>>\n%s\n", strUser(user1), strUser(user2))
+			user1.comparedResult = &comparedResult
+			user2.comparedResult = user1.comparedResult
+			fmt.Printf(comparedResult)
+		}
+	}
+
+	for _, user2 := range users2 {
+		if user2.comparedState == 0 {
+			user2.comparedState = 1
+		}
+	}
+}
+
+func strUser(u *User) string {
+	return nullStr(u.userId) + "," + nullStr(u.mobile) +
+		"," + nullStr(u.openid) + "," + nullStr(u.merchantId) +
+		"," + nullStr(u.createTime) + "," + nullStr(u.effective)
+}
+
+func nullStr(str sql.NullString) string {
+	if str.Valid {
+		return str.String
+	} else {
+		return "NULL"
+	}
+
+}
+
+func makeMap(users []*User) map[string]*User {
+	mapa := make(map[string]*User)
+	for _, v := range users {
+		mapa[v.userId.String] = v
+	}
+
+	return mapa
+}
+
+func printRows(users []*User) {
+	for _, v := range users {
+		fmt.Println(v)
+	}
+}
+
+func printMap(users map[string]*User) {
+	for k, v := range users {
+		fmt.Println("k:", k, "v:", v)
+	}
+}
+
+func getDb(dataSourceName string) *sql.DB {
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func insertUser(db *sql.DB, user *User) {
+	stmt, err := db.Prepare("insert into tr_f_user(" +
+		"user_id, mobile, openid, merchant_id, create_time, effective) " +
+		"values(?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := stmt.Exec(user.userId, user.mobile, user.openid,
+		user.merchantId, user.createTime, user.effective)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+}
+
+func queryRows(db *sql.DB) (users []*User) {
+	rows, err := db.Query("select user_id, mobile, openid, " +
+		"merchant_id, create_time, effective from tr_f_user")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		row := new(User)
+
+		err := rows.Scan(&row.userId, &row.mobile, &row.openid,
+			&row.merchantId, &row.createTime, &row.effective)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		users = append(users, row)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
