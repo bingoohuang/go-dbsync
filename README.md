f125e44a2f26ca518449bd00087b29cfec792d9e
bingoohuang <bingoo.huang@gmail.com>
2016-08-30 11:05:15 +0800

    prepare to replicate mysql database on application level

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ synchronize the database tables
 
 1. 获取MySQL驱动 `go get github.com/go-sql-driver/mysql`
 2. 本地编译 `go build src/user-sync.go` 
-3. Linux 编译 `GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o user-sync.linux src/user-sync.go`
+3. 本地编译Linux版本(bash) `GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o user-sync.linux src/user-sync.go`
 
 只能同步tr_f_user表的数据。
 同步原理：比较两边数据，没有的补充上，有的但是不一样的，控制台打印。
@@ -20,15 +20,27 @@ synchronize the database tables
 2. 获取nodb `go get github.com/lunny/nodb`
 3. 获取toml `go get github.com/BurntSushi/toml`
 4. 本地编译 `go build src/dbsync.go` 
+5. 本地编译Linux版本(bash) `GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o dbsync.linux src/dbsync.go`
+
 
 可以同步指定表的数据,例如:
 
 ```
-~/g/go-dbsync > ./dbsync "root:my-secret-pw@tcp(192.168.99.100:13306)/dba" "root:my-secret-pw@tcp(192.168.99.100:13306)/dbb" tr_f_user
+~/g/go-dbsync > ./dbsync config/dbsync.toml
 <<<map[openid:NULL merchant_id:0 create_time:2016-05-16 10:34:22 effective:1 user_id:a045719884460976128 mobile:18231869455]
 >>>map[create_time:2016-05-16 10:34:22 effective:1 user_id:a045719884460976128 mobile:18231869455 openid: merchant_id:0]
 <<<map[create_time:2016-08-04 14:36:36 effective:1 user_id:b217018113249683456 mobile:18231684453 openid: merchant_id:18D72F678EFA]
 >>>map[user_id:b217018113249683456 mobile:18231684453 openid:NULL merchant_id:18D72F678EFA create_time:2016-08-04 14:36:36 effective:1]
 Start to merge left tr_f_user, merged 30 rows
 Start to merge right tr_f_user, merged 15 rows
+```
+
+### 配置文件格式
+
+```toml
+# 同步的两个库配置
+Db1 = "root:my-secret-pw@tcp(192.168.99.100:13306)/dba"
+Db2 = "root:my-secret-pw@tcp(192.168.99.100:13306)/dbb"
+# 需要同步的表,可以指定多个
+SyncTables = [ "tr_f_user" ]
 ```
---
+++ src/dbreplic.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "github.com/jasonlvhit/gocron"
+    "./myutil"
+    "./mydb"
+    "./mynodb"
+    "os"
+    "github.com/BurntSushi/toml"
+    _ "github.com/go-sql-driver/mysql"
+    "fmt"
+)
+
+/*
+MySQL数据库单向同步.
+1) 查询表名、表行数：
+select TABLE_NAME,TABLE_ROWS from information_schema.tables where table_schema = DATABASE();
+2) 查询列名和列类型
+SELECT column_name , column_type FROM information_schema. COLUMNS
+WHERE table_schema = database() AND table_name = 'cats' ORDER BY ORDINAL_POSITION;
+3) 查询表主键
+SELECT k.COLUMN_NAME FROM information_schema.table_constraints t LEFT JOIN information_schema.key_column_usage k USING( constraint_name , table_schema , table_name)
+WHERE t.constraint_type = 'PRIMARY KEY'
+AND t.table_schema = DATABASE() AND t.table_name = 'cats';
+4）建表
+SHOW CREATE TABLE dba.cats;
+DESCRIBE cats;
+ */
+
+func main() {
+    dbReplicConfig := readDbReplicConfig()
+    dbFrom := mydb.GetDb(dbReplicConfig.DbFrom)
+    defer dbFrom.Close()
+    dbTo := mydb.GetDb(dbReplicConfig.DbTo)
+    defer dbTo.Close()
+
+    nodb, tempDir, _ := mynodb.OpenTemp()
+    defer os.RemoveAll(tempDir)
+
+    gocron.Every(5).Seconds().Do(mainTask, dbReplicConfig, nodb, dbFrom, dbTo)
+    <-gocron.Start()
+}
+
+func mainTask(dbReplicConfig DbReplicConfig, nodb *mynodb.Nodb, dbFrom, dbTo *mydb.Db) {
+    fmt.Println(dbReplicConfig)
+}
+
+type DbReplicConfig struct {
+    DbFrom        string `toml:"dbFrom"`
+    DbTo          string `toml:"dbTo"`
+    ExcludeTables []string `toml:"excludeTables"`
+}
+
+func readDbReplicConfig() DbReplicConfig {
+    fpath := "dbreplic.toml"
+    if len(os.Args) > 1 {
+        fpath = os.Args[1]
+    }
+
+    dbReplicConfig := DbReplicConfig{}
+    if _, err := toml.DecodeFile(fpath, &dbReplicConfig); err != nil {
+        myutil.CheckErr(err)
+    }
+
+    return dbReplicConfig
+}
