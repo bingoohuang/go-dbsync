b07af37ee3a9332b6e7ce94b23097e14df3893f0
bingoohuang <bingoo.huang@gmail.com>
2017-03-01 19:37:18 +0800

    support config

--- src/go-blackcat-web.go
+++ src/go-blackcat-web.go
@@ -1,21 +1,50 @@
 package main
 
 import (
-	"log"
+	"./myutil"
 
+	"log"
 	"github.com/gocql/gocql"
 	"gopkg.in/kataras/iris.v6"
 	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
 	"strings"
+	"os"
+	"github.com/BurntSushi/toml"
+	"strconv"
 )
 
+type Config struct {
+	CassandraHosts []string
+	CassandraPort  int
+	ListenPort     int
+}
+
+func ReadConfig() Config {
+	fpath := "go-blackcat-web.toml"
+	if len(os.Args) > 1 {
+		fpath = os.Args[1]
+	}
+
+	config := Config{}
+	if _, err := toml.DecodeFile(fpath, &config); err != nil {
+		myutil.CheckErr(err)
+	}
+	//fmt.Println(config)
+
+	return config
+}
+
+var config Config
+
 func main() {
+	config = ReadConfig()
+
 	app := iris.New()
 	app.Adapt(httprouter.New())
 
 	app.Get("/", IndexHandler)
 	app.Get("/query/:traceids", BlackcatHandler)
-	app.Listen(":8181")
+	app.Listen(":" + strconv.Itoa(config.ListenPort))
 }
 
 func BlackcatHandler(ctx *iris.Context) {
@@ -25,19 +54,20 @@ func BlackcatHandler(ctx *iris.Context) {
 	session, _ := CreateSession()
 	defer session.Close()
 
-	cql := `SELECT tspretty,linkid,msgtype,msg FROM event_trace WHERE traceid = ?`;
+	cql := `SELECT tspretty,linkid,msgtype,msg FROM event_trace WHERE traceid = ?`
 
 	for _, traceid := range traceidArr {
-		ctx.Writef("==========traceid:%s\n", traceid);
+		ctx.Writef("==========traceid:%s\n", traceid)
 		ExecuteQuery(session, cql, traceid, ctx)
 	}
 
 }
 func CreateSession() (*gocql.Session, error) {
-	cluster := gocql.NewCluster("120.77.219.149")
-	// cluster := gocql.NewCluster("127.0.0.1")
+	cluster := gocql.NewCluster(config.CassandraHosts...)
 	cluster.Keyspace = "blackcat"
 	cluster.Consistency = gocql.Quorum
+	cluster.Port = config.CassandraPort
+
 	return cluster.CreateSession()
 }
 
@@ -57,10 +87,9 @@ func IndexHandler(ctx *iris.Context) {
 	ctx.WriteString(`
 <html>
 <body>
-<textarea placeholder="input traceids, like 210486120201746432X210486119419508736" id="traceids" style="width:1000;">
+<textarea placeholder="input traceids, like 210486120201746432X210486119419508736" id="traceids" style="width:1000">
 </textarea>
 <br/>
-
 <input type="button" value="Query" onclick="queryLogs()">
 <input type="button" value="Clear Result" onclick="clearLogs()">
 &nbsp;&nbsp;&nbsp; <input type="checkbox" id="preservResult">Preserve Result
@@ -68,7 +97,7 @@ func IndexHandler(ctx *iris.Context) {
 <div id="output"></div>
 </body>
 <script>
-var $ = function(id) {
+function $(id) {
 	return document.getElementById(id)
 }
 
@@ -78,38 +107,26 @@ function clearLogs() {
 
 function queryLogs() {
 	minAjax({
-		url:"/query/" + $("traceids").value,//request URL
-		type:"GET", //Request type GET/POST
-		data:{
-
-		},
+		url:"/query/" + $("traceids").value,
+		type:"GET",
+		data:{},
 		success: function(data){
 			var old = $("output").innerText
-			if ($("preservResult").checked) {
-				$("output").innerText = data +  "**************\n"+ old
-			} else {
-				$("output").innerText = data
-			}
+			var oldData =  $("preservResult").checked ? old : ""
+			$("output").innerText = data + oldData
 		}
-	});
+	})
 }
 
-/*|--minAjax.js--|
-  |--(A Minimalistic Pure JavaScript Header for Ajax POST/GET Request )--|
+/*|--(A Minimalistic Pure JavaScript Header for Ajax POST/GET Request )--|
   |--Author : flouthoc (gunnerar7@gmail.com)(http://github.com/flouthoc)--|
-  |--Contributers : Add Your Name Below--|
   */
 function initXMLhttp() {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-		//code for IE7,firefox chrome and above
-		xmlhttp = new XMLHttpRequest();
-	} else {
-		//code for Internet Explorer
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	if (window.XMLHttpRequest) { // code for IE7,firefox chrome and above
+		return new XMLHttpRequest()
+	} else { // code for Internet Explorer
+		return new ActiveXObject("Microsoft.XMLHTTP")
 	}
-
-	return xmlhttp;
 }
 
 function minAjax(config) {
@@ -118,72 +135,49 @@ function minAjax(config) {
 	url:"reqesting URL"
 	type:"GET or POST"
 	method: "(OPTIONAL) True for async and False for Non-async | By default its Async"
-	debugLog: "(OPTIONAL)To display Debug Logs | By default it is false"
 	data: "(OPTIONAL) another Nested Object which should contains reqested Properties in form of Object Properties"
 	success: "(OPTIONAL) Callback function to process after response | function(data,status)"
 	*/
 
 	if (!config.method) {
-		config.method = true;
+		config.method = true
 	}
 
-	if (!config.debugLog) {
-		config.debugLog = false;
-	}
-
-	var xmlhttp = initXMLhttp();
+	var xmlhttp = initXMLhttp()
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			if (config.success) {
-				config.success(xmlhttp.responseText, xmlhttp.readyState);
-			}
-
-			if (config.debugLog == true) {
-				console.log("SuccessResponse");
-				console.log("Response Data:" + xmlhttp.responseText);
-			}
-
-		} else {
-			if (config.debugLog == true)
-				console.log("FailureResponse --> State:" + xmlhttp.readyState + "Status:" + xmlhttp.status);
+			config.success(xmlhttp.responseText, xmlhttp.readyState)
 		}
 	}
 
-	var sendString = [],
-		sendData = config.data;
-	if( typeof sendData === "string" ){
-		var tmpArr = String.prototype.split.call(sendData,'&');
+	var sendString = [], sendData = config.data
+	if ( typeof sendData === "string" ){
+		var tmpArr = String.prototype.split.call(sendData,'&')
 		for(var i = 0, j = tmpArr.length; i < j; i++){
-			var datum = tmpArr[i].split('=');
-			sendString.push(encodeURIComponent(datum[0]) + "=" + encodeURIComponent(datum[1]));
+			var datum = tmpArr[i].split('=')
+			sendString.push(encodeURIComponent(datum[0]) + "=" + encodeURIComponent(datum[1]))
 		}
-	}else if( typeof sendData === 'object' && !( sendData instanceof String || (FormData && sendData instanceof FormData) ) ){
+	} else if( typeof sendData === 'object' && !( sendData instanceof String || (FormData && sendData instanceof FormData) ) ){
 		for (var k in sendData) {
-			var datum = sendData[k];
-			if( Object.prototype.toString.call(datum) == "[object Array]" ){
+			var datum = sendData[k]
+			if ( Object.prototype.toString.call(datum) == "[object Array]" ){
 				for(var i = 0, j = datum.length; i < j; i++) {
-						sendString.push(encodeURIComponent(k) + "[]=" + encodeURIComponent(datum[i]));
+					sendString.push(encodeURIComponent(k) + "[]=" + encodeURIComponent(datum[i]))
 				}
-			}else{
-				sendString.push(encodeURIComponent(k) + "=" + encodeURIComponent(datum));
+			} else {
+				sendString.push(encodeURIComponent(k) + "=" + encodeURIComponent(datum))
 			}
 		}
 	}
-	sendString = sendString.join('&');
+	sendString = sendString.join('&')
 
 	if (config.type == "GET") {
-		xmlhttp.open("GET", config.url + "?" + sendString, config.method);
-		xmlhttp.send();
-
-		if (config.debugLog == true)
-			console.log("GET fired at:" + config.url + "?" + sendString);
+		xmlhttp.open("GET", config.url + "?" + sendString, config.method)
+		xmlhttp.send()
 	} else if (config.type == "POST") {
-		xmlhttp.open("POST", config.url, config.method);
-		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send(sendString);
-
-		if (config.debugLog == true)
-			console.log("POST fired at:" + config.url + " || Data:" + sendString);
+		xmlhttp.open("POST", config.url, config.method)
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+		xmlhttp.send(sendString)
 	}
 }
 </script>
