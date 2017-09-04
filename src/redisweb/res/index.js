cf91ffe3cc50aa34168967e2eda04e66aa1302fa
bingoohuang <bingoo.huang@gmail.com>
2017-09-04 22:48:44 +0800

    refactor js

--- src/redisweb/res/index.js
+++ src/redisweb/res/index.js
@@ -167,60 +167,34 @@ $(function () {
         var contentHtml = '<div><span class="key">Add another key</span></div>'
         contentHtml += '<table class="contentTable">' +
             '<tr><td class="titleCell">Type:</td><td colspan="2"><select name="type" id="type">' +
-            '<option value="string">String</option>' +
-            '<option value="hash">Hash</option>' +
-            '<option value="list">List</option>' +
-            '<option value="set">Set</option>' +
-            '<option value="zset">Sorted Set</option>' +
+            '<option value="string">String</option><option value="hash">Hash</option><option value="list">List</option><option value="set">Set</option><option value="zset">Sorted Set</option>' +
             '</select></td></tr>' +
             '<tr><td class="titleCell">Key:</td><td colspan="2"><input id="key" placeholder="input the new key"></td></tr>' +
             '<tr><td class="titleCell">TTL:</td><td colspan="2"><input id="ttl" placeholder="input the expired time, like 1d/1h/10s/-1s"></td></tr>' +
             '<tr><td class="titleCell">Format:</td><td colspan="2"><select name="format" id="format">' +
-            '<option value="String">String</option>' +
-            '<option value="JSON">JSON</option>' +
-            '<option value="Quoted">Quoted</option>' +
+            '<option value="String">String</option><option value="JSON">JSON</option><option value="Quoted">Quoted</option>' +
             '</select></td></tr>' +
             '<tr><td class="titleCell">Value:</td><td colspan="2"><span class="valueSave sprite sprite-save"></span></td></tr>'
 
         contentHtml += '<tr class="newKeyTr string"><td colspan="2"><textarea id="code"></textarea></td></tr>'
 
-        contentHtml += '<tr class="newKeyTr hash"><td class="titleCell">Field</td><td colspan="2" class="titleCell">Value</td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
+        contentHtml += '<tr class="newKeyTr hash"><td class="titleCell">Field</td><td colspan="2" class="titleCell">Value</td></tr>'
 
-        contentHtml += '<tr class="newKeyTr list set"><td class="titleCell">#</td><td colspan="2" class="titleCell">Value</td></tr>' +
-            '<tr class="newKeyTr list set"><td>0</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>1</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>2</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>3</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>4</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>5</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>6</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>7</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>8</td><td colspan="2" contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr list set"><td>9</td><td colspan="2" contenteditable="true"></td></tr>'
+        for (var i = 0; i < 10; ++i) {
+            contentHtml += '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
+        }
 
-        contentHtml += '<tr class="newKeyTr zset"><td class="titleCell">#</td><td class="titleCell">Score</td><td class="titleCell">Value</td></tr>' +
-            '<tr class="newKeyTr zset"><td>0</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>1</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>2</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>3</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>4</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>5</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>6</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>7</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>8</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '<tr class="newKeyTr zset"><td>9</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
-            '</table>'
+        contentHtml += '<tr class="newKeyTr list set"><td class="titleCell">#</td><td colspan="2" class="titleCell">Value</td></tr>'
+        for (var i = 0; i < 10; ++i) {
+            contentHtml += '<tr class="newKeyTr list set"><td>' + i + '</td><td colspan="2" contenteditable="true"></td></tr>'
+        }
 
+        contentHtml += '<tr class="newKeyTr zset"><td class="titleCell">#</td><td class="titleCell">Score</td><td class="titleCell">Value</td></tr>'
+        for (var i = 0; i < 10; ++i) {
+            contentHtml += '<tr class="newKeyTr zset"><td>' + i + '</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>'
+        }
+
+        contentHtml += '</table>'
         contentHtml += '<button id="addMoreRowsBtn">Add More Rows</button>'
 
         $('#frame').html(contentHtml)
@@ -235,11 +209,11 @@ $(function () {
             var moreRows = ''
             for (var i = 0; i < 10; ++i) {
                 if (type == 'hash') {
-                    moreRows +=  '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
-                } else if (type =='list' || type == 'set') {
+                    moreRows += '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
+                } else if (type == 'list' || type == 'set') {
                     moreRows += '<tr class="newKeyTr list set"><td>' + (startRowNum + i) + '</td><td colspan="2" contenteditable="true"></td></tr>'
                 } else if (type == 'zset') {
-                    moreRows += '<tr class="newKeyTr zset"><td>' + (startRowNum + i) +'</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>'
+                    moreRows += '<tr class="newKeyTr zset"><td>' + (startRowNum + i) + '</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>'
                 }
             }
             $(moreRows).appendTo($('.contentTable'))
