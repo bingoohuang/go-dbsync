d79b5fba44047c17137ff1591e8317626fd9cc7f
bingoohuang <bingoo.huang@gmail.com>
2017-09-04 22:45:34 +0800

    add support list/set/zset/hash in html

--- src/redisweb/res/index.js
+++ src/redisweb/res/index.js
@@ -165,30 +165,97 @@ $(function () {
 
     $('#addKey').click(function () {
         var contentHtml = '<div><span class="key">Add another key</span></div>'
-        contentHtml += '<table>' +
-            '<tr><td>Type:</td><td><select name="type" id="type">' +
+        contentHtml += '<table class="contentTable">' +
+            '<tr><td class="titleCell">Type:</td><td colspan="2"><select name="type" id="type">' +
             '<option value="string">String</option>' +
             '<option value="hash">Hash</option>' +
             '<option value="list">List</option>' +
             '<option value="set">Set</option>' +
-            '<option value="zset">ZSet</option>' +
+            '<option value="zset">Sorted Set</option>' +
             '</select></td></tr>' +
-            '<tr><td>Key:</td><td><input id="key" placeholder="input the new key"></td></tr>' +
-            '<tr><td>TTL:</td><td><input id="ttl" placeholder="input the expired time, like 1d/1h/10s/-1s"></td></tr>' +
-            '<tr><td>Format:</td><td><select name="format" id="format">' +
+            '<tr><td class="titleCell">Key:</td><td colspan="2"><input id="key" placeholder="input the new key"></td></tr>' +
+            '<tr><td class="titleCell">TTL:</td><td colspan="2"><input id="ttl" placeholder="input the expired time, like 1d/1h/10s/-1s"></td></tr>' +
+            '<tr><td class="titleCell">Format:</td><td colspan="2"><select name="format" id="format">' +
             '<option value="String">String</option>' +
             '<option value="JSON">JSON</option>' +
             '<option value="Quoted">Quoted</option>' +
             '</select></td></tr>' +
-            '<tr><td>Value:</td><td><span class="valueSave sprite sprite-save"></span></td></tr>' +
-            '<tr><td colspan="2"><textarea id="code"></textarea></td></tr>' +
+            '<tr><td class="titleCell">Value:</td><td colspan="2"><span class="valueSave sprite sprite-save"></span></td></tr>'
+
+        contentHtml += '<tr class="newKeyTr string"><td colspan="2"><textarea id="code"></textarea></td></tr>'
+
+        contentHtml += '<tr class="newKeyTr hash"><td class="titleCell">Field</td><td colspan="2" class="titleCell">Value</td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
+
+        contentHtml += '<tr class="newKeyTr list set"><td class="titleCell">#</td><td colspan="2" class="titleCell">Value</td></tr>' +
+            '<tr class="newKeyTr list set"><td>0</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>1</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>2</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>3</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>4</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>5</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>6</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>7</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>8</td><td colspan="2" contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr list set"><td>9</td><td colspan="2" contenteditable="true"></td></tr>'
+
+        contentHtml += '<tr class="newKeyTr zset"><td class="titleCell">#</td><td class="titleCell">Score</td><td class="titleCell">Value</td></tr>' +
+            '<tr class="newKeyTr zset"><td>0</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>1</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>2</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>3</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>4</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>5</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>6</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>7</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>8</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
+            '<tr class="newKeyTr zset"><td>9</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>' +
             '</table>'
+
+        contentHtml += '<button id="addMoreRowsBtn">Add More Rows</button>'
 
         $('#frame').html(contentHtml)
 
+        $('tr.newKeyTr').hide()
+        $('tr.string').show()
+        $('#addMoreRowsBtn').hide().click(function () {
+            var type = $('#type').val()
+            var rows = $('tr.' + type)
+            var startRowNum = rows.length - 1
+
+            var moreRows = ''
+            for (var i = 0; i < 10; ++i) {
+                if (type == 'hash') {
+                    moreRows +=  '<tr class="newKeyTr hash"><td contenteditable="true"></td><td colspan="2" contenteditable="true"></td></tr>'
+                } else if (type =='list' || type == 'set') {
+                    moreRows += '<tr class="newKeyTr list set"><td>' + (startRowNum + i) + '</td><td colspan="2" contenteditable="true"></td></tr>'
+                } else if (type == 'zset') {
+                    moreRows += '<tr class="newKeyTr zset"><td>' + (startRowNum + i) +'</td><td contenteditable="true"></td><td contenteditable="true"></td></tr>'
+                }
+            }
+            $(moreRows).appendTo($('.contentTable'))
+        })
+
+
+        $('#type').change(function () {
+            var type = $('#type').val()
+            $('tr.newKeyTr').hide()
+            $('tr.' + type).show()
+            $('#addMoreRowsBtn').toggle(type != 'string')
+        })
+
         $('#format').change(function () {
             codeMirror = null
-            if ($(this).val() == 'JSON') {
+            if ($(this).val() == 'JSON' && $('#type').val() == 'string') {
                 codeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
                     mode: 'application/json',
                     lineNumbers: true,
@@ -248,7 +315,7 @@ $(function () {
         }
 
         var contentHtml = '<div><span class="key">' + key + '</span></div>'
-        contentHtml += '<table id="contentTable">' +
+        contentHtml += '<table class="contentTable">' +
             '<tr><td class="titleCell">Type:</td><td colspan="2">' + type + '</td></tr>' +
             '<tr><td class="titleCell">TTL:</td><td colspan="2">' + ttl + '</td></tr>' +
             '<tr><td class="titleCell">Encoding:</td><td colspan="2">' + encoding + '</td></tr>' +
